af0a2e7848e74e50f8a26ee878ecb6776646cb12
lexeach <80155340+lexeach@users.noreply.github.com>
2026-04-07 21:57:56 +0530

    Update LeadRoutes.js

--- src/routes/LeadRoutes.js
+++ src/routes/LeadRoutes.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const leadController =
-  require("../controllers/leadController");
+const leadController = require(
+  "../controllers/leadController"
+);
 
+/* CREATE NEW LEAD */
 router.post(
   "/create",
-  (req, res) =>
-    leadController.createLead(
-      req,
-      res
-    )
+  leadController.createLead
+);
+
+/* GET ALL LEADS */
+router.get(
+  "/all",
+  leadController.getAllLeads
 );
 
 module.exports = router;
