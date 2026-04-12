e8870727b02b105f0eee7bdc9a8cab9ce30d47ff
lexeach <80155340+lexeach@users.noreply.github.com>
2026-04-12 08:35:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,13 @@ app.use(
     ]
   })
 );
-
+app.use((req, res, next) => {
+  console.log(
+    `📩 ${req.method} ${req.originalUrl}`,
+    req.body
+  );
+  next();
+});
 app.use(express.json());
 app.use(
   express.urlencoded({
