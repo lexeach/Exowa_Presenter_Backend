7a13b8ee90cf97e96294445a42e0abfe19af7c04
lexeach <80155340+lexeach@users.noreply.github.com>
2026-04-20 19:42:57 +0530

    Update vobizCallRoutes.js

--- src/routes/vobizCallRoutes.js
+++ src/routes/vobizCallRoutes.js
@@ -3,12 +3,5 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  answerCall,
-  realtimeVoice,
-} = require("../controllers/voiceController");
-
-router.post("/answer", answerCall);
-router.post("/realtime", realtimeVoice);
 
 module.exports = router;
