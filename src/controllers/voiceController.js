05548b00b570de9362c4e1ea5b8fae75fa3951a2
lexeach <80155340+lexeach@users.noreply.github.com>
2026-04-21 21:06:18 +0530

    Update voiceController.js

--- src/controllers/voiceController.js
+++ src/controllers/voiceController.js
@@ -4,22 +4,21 @@ exports.answerCall = async (req, res) => {
 
     const responseXML = `
 <Response>
-  <Speak language="hi-IN" voice="WOMAN">
-    नमस्ते, मैं Exowa AI assistant बोल रही हूँ।
-  </Speak>
 
   <GetInput
     action="https://exowa-presenter-backend.onrender.com/api/vobiz/process-slot"
     method="POST"
     inputType="speech"
     language="hi-IN"
-    timeout="5"
+    timeout="7"
     speechTimeout="auto"
   >
     <Speak language="hi-IN" voice="WOMAN">
+      नमस्ते, मैं Exowa AI assistant बोल रही हूँ।
       क्या आप मुझे सुन पा रहे हैं? कृपया कुछ बोलिए।
     </Speak>
   </GetInput>
+
 </Response>`;
 
     res.set("Content-Type", "text/xml");
